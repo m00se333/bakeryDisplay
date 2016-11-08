874fa7aa5f34005b981b1e274e01836e73075f35
m00se333 <moosecunningham@gmail.com>
2016-11-07 19:00:17 -0800

    changed the localhost port attached to the PORT variable.

--- devServer.js
+++ devServer.js
@@ -2,7 +2,7 @@ var path = require('path');
 var express = require('express');
 var webpack = require('webpack');
 var config = require('./webpack.config.dev');
-var PORT = process.env.PORT || 8080
+var PORT = process.env.PORT || 7770
 
 var app = express();
 var compiler = webpack(config);
@@ -18,7 +18,7 @@ app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(PORT, function(err) {
+app.listen(PORT, "localhost", function(err) {
   if (err) {
     console.log(err);
     return;
