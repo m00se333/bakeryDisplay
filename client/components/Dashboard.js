cff0a8b5f2fd2d854064512f3e3d1e776c9dc058
m00se333 <moosecunningham@gmail.com>
2016-11-23 20:07:16 -0800

    Started on the order form.

--- client/components/Dashboard.js
+++ client/components/Dashboard.js
@@ -7,7 +7,41 @@ const Dashboard = React.createClass({
     return(
       <div className="dashContainer">
 
-        <div className="box"></div>
+        <form className="orderForm">
+          
+          <span>Select your item</span>
+          <select className="pastry-type">
+            <option> select </option>
+            <option>Cake small</option>
+            <option>Cake large</option>
+            <option>Cupcakes 12 count</option>
+            <option>Cupcakes 24 count</option>
+          </select>
+          
+          <span>What's the occasion?</span>
+          <select className="pastry-type">
+            <option> select </option>
+            <option>Birthday</option>
+            <option>Office Party</option>
+            <option>Special Request</option>
+            <option>Other</option>
+          </select>
+
+          <span>A phone number I can reach you at</span>
+          <input type="text" placeholder="(###) - ### - ####"/>
+
+          <span>Your email address</span>
+          <input type="text"/>
+
+          <input type="button" value="Submit order"/>
+        </form>
+
+        <div className="orderPreview">
+          <span>Item:</span>
+          <span>For:</span>
+          <span>Contact (phone):</span>
+          <span>Contact (email):</span>          
+        </div>
 
       </div>
     )
